eec9759e92a2a1e1ed7bf8bf1da03ac30461a315
Miguel <miguel@debloat.us>
2023-02-10 22:56:51 -0500

    moles have a secondary step after selecting them

--- src/components/Moles.jsx
+++ src/components/Moles.jsx
@@ -1,24 +1,48 @@
+import {useState} from "react";
 import styled from "styled-components";
 import putin from "../media/putin1.png";
 import putinHitted from "../media/putin2.png";
+import trump from "../media/trump1.png";
+import trumpHitted from "../media/trump2.png";
 
+const MoleContainer = styled.div`
+	position: relative;
+  height: 20vh;
+  width: 40vh;
+	cursor: pointer;
+`;
+const PlayButton = styled.div`
+  position: absolute;
+	display: ${(props) => props.active ? 'flex' : 'none'};
+	//display: flex;
+	align-items: center;
+	justify-content: center;
+	text-align: center;
+  width: 100%;
+  height: 100%;
+	color: greenyellow;
+	font-weight: bold;
+	font-size: large;
+  background: rgba(0,0,0,0.5);
+`;
 const ImageContainer = styled.img`
-	width: 20vh;
-	height: 20vh;
-`
+  width: 20vh;
+  height: 20vh;
+`;
 const Moles = ({setMoleHitted, setMole}) => {
-	const setPutin = () => {
-		setMole(putin)
-		setMoleHitted(putinHitted)
-	}
-	return (
-		<div>
-			<div onClick={() => setPutin()}>
-				<ImageContainer src={putin} />
-				<ImageContainer src={putinHitted} />
-			</div>
-
-		</div>
-)
-}
-export default Moles
+	const moles = [[putin, putinHitted], [trump, trumpHitted]];
+	const [playVisibility, setPlayVisibility] = useState(null);
+	const setTheMole = (index) => {
+		setMole(moles[index][0]);
+		setMoleHitted(moles[index][1]);
+	};
+	return (<div>
+		{moles.map((moleSet, index) =>
+			           <MoleContainer key={index} onClick={() =>  setPlayVisibility(index)}>
+				           <PlayButton onClick={() => setTheMole(index)} active={index === playVisibility}>Play</PlayButton>
+				           <ImageContainer src={moleSet[0]}/>
+				           <ImageContainer src={moleSet[1]}/>
+			           </MoleContainer>)}
+	</div>);
+};
+export default Moles;
