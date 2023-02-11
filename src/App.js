cfc15496b1cfd86d0ff0a72546672620aca20cea
elijahb22 <elibusick11@gmail.com>
2023-02-11 15:41:58 -0500

    fix background and hover buttons

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Game from "./components/Game";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Play from "./components/Play";
-
 function App() {
   const testUser = "Miguel"
   const testJwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
@@ -15,7 +14,7 @@ function App() {
   return (
     <div>
       <Header/>
-      <Routes>
+      <Routes href="https://th.bing.com/th/id/R.db44bf29c16cebecd6592b34dab85a52?rik=PcV2Dl135qBx4A&riu=http%3a%2f%2fgetwallpapers.com%2fwallpaper%2ffull%2f4%2f9%2f0%2f883392-shiny-chrome-wallpaper-1920x1080-smartphone.jpg&ehk=txtffGMVFxieZ9W41KyV%2fKLwR7smb03MdGyzbnuokvI%3d&risl=&pid=ImgRaw&r=0">
         <Route path="/" exact  element={<Home/>}/>
         {/*<Route path="/login" element={<Login setJwt/>}/>*/}
         <Route path={"/play"} element={<Play jwt={jwt} />}/>
--- src/components/Level.jsx
+++ src/components/Level.jsx
@@ -1,30 +1,46 @@
 import styled from "styled-components";
-const ButtonContainer = styled.div`
+
+var ButtonContainer = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
+
 `
 
-const Button = styled.button`
+var Button = styled.button`
 	color: blue;
-	background: black;
+	background: red;
 	margin: 5vh;
 	width: 20vw;
+	&:hover  {
+		margin: 5vh;
+		width: 20vw;
+		color: rgba(0, 255, 0, 1);
+		background: blue;
+  		box-shadow: 0 5px 15px rgba(0, 92, 0, 1);
+		transform: scale(1.2) translate(0%, 0%);
+	}
+
 `
+
+
+
 const Level = ({setLevel}) => {
 
 	return (
 		<ButtonContainer>
+			<h2>Choose a Difficulty?</h2>
+			
 			<Button onClick={() => setLevel(3)}>
-				Easy
+			Easy
 			</Button>
+		
 			<Button onClick={() => setLevel(5)}>
 				Medium
 			</Button>
 			<Button onClick={() => setLevel(7)}>
 				Hard
 			</Button>
-
 		</ButtonContainer>
 	)
 
